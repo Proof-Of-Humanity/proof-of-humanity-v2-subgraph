addc5122aafffe03dff3dcf08c101bda5f13ccff
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2024-01-24 19:23:12 +0200

    fix: vouch processing

--- utils/hardcoded.ts
+++ utils/hardcoded.ts
@@ -2,7 +2,7 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { ProofOfHumanity as ProofOfHumanityContract } from "../generated/ProofOfHumanity/ProofOfHumanity";
 
 export const ProofOfHumanityAddress = Address.fromBytes(
-  Bytes.fromHexString("0x29defF3DbEf6f79ef20d3fe4f9CFa0547acCeC0D")
+  Bytes.fromHexString("0xB6412c84eC958cafcC80B688d6F473e399be488f")
 );
 
 export const ProofOfHumanity = ProofOfHumanityContract.bind(
