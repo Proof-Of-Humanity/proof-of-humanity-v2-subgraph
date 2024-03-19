58a77d56de10d59e2c984bfdc842c5aa973c2c63
Martin <martin.moguillansky@gmail.com>
2024-03-19 18:02:23 -0300

    Update of PoH contract address in gnosis

--- utils/hardcoded.ts
+++ utils/hardcoded.ts
@@ -2,7 +2,7 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { ProofOfHumanity as ProofOfHumanityContract } from "../generated/ProofOfHumanity/ProofOfHumanity";
 
 export const ProofOfHumanityAddress = Address.fromBytes(
-  Bytes.fromHexString("0x29defF3DbEf6f79ef20d3fe4f9CFa0547acCeC0D")
+  Bytes.fromHexString("0x4a594f0e73223c9a1CE0EfC16da92fFaA193a612")
 );
 
 export const ProofOfHumanity = ProofOfHumanityContract.bind(
