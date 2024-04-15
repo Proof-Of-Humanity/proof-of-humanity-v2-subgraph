2964d9d18a279d64dd8c8d4210ae3113e403f23f
Martin <martin.moguillansky@gmail.com>
2024-04-15 15:12:35 -0300

    thegraph auth scripts

--- utils/hardcoded.ts
+++ utils/hardcoded.ts
@@ -2,7 +2,7 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { ProofOfHumanity as ProofOfHumanityContract } from "../generated/ProofOfHumanity/ProofOfHumanity";
 
 export const ProofOfHumanityAddress = Address.fromBytes(
-  Bytes.fromHexString("0x2505C87AA36d9ed18514Ea7473Ac58aeDeb50849")
+  Bytes.fromHexString("0x29defF3DbEf6f79ef20d3fe4f9CFa0547acCeC0D")
 );
 
 export const ProofOfHumanity = ProofOfHumanityContract.bind(
