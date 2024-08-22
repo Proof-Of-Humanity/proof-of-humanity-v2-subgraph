5dacfdb21ae95f397aec7795ece09d3570482727
Martin <martin.moguillansky@gmail.com>
2024-08-22 15:03:18 -0300

    update: endBlock configuration
    - endBlock is configured to stop syncing the legacy contract (pohV1) on the block where the ForkModule has been created. No v1-profile created after v2 deployments will be migrated into v2 subgraphs nor be readable at v2-frontend. (Smart contracts has their own filtering configuration)

--- utils/hardcoded.ts
+++ utils/hardcoded.ts
@@ -2,7 +2,7 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { ProofOfHumanity as ProofOfHumanityContract } from "../generated/ProofOfHumanity/ProofOfHumanity";
 
 export const ProofOfHumanityAddress = Address.fromBytes(
-  Bytes.fromHexString("0xECd1823b3087acEE3C77928b1959c08d31A8F20e")
+  Bytes.fromHexString("0x87c5c294C9d0ACa6b9b2835A99FE0c9A444Aacc1")
 );
 
 export const ProofOfHumanity = ProofOfHumanityContract.bind(
