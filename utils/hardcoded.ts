bbea553b3da10c27b746f8466815fbf394d71bc4
Martin <martin.moguillansky@gmail.com>
2024-09-28 17:00:42 -0300

    fix: non-nullable requester fund

--- utils/hardcoded.ts
+++ utils/hardcoded.ts
@@ -2,7 +2,7 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { ProofOfHumanity as ProofOfHumanityContract } from "../generated/ProofOfHumanity/ProofOfHumanity";
 
 export const ProofOfHumanityAddress = Address.fromBytes(
-  Bytes.fromHexString("0x2505C87AA36d9ed18514Ea7473Ac58aeDeb50849")
+  Bytes.fromHexString("0x6cbEdC1920090EA4F28A38C1CD61c8D37b2cc323")
 );
 
 export const ProofOfHumanity = ProofOfHumanityContract.bind(
