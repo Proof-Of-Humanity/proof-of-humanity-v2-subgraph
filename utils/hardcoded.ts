1b738bccddc94bfeb6db2a341ee276b7d88e7772
Martin <martin.moguillansky@gmail.com>
2024-10-11 18:05:42 -0300

    contribution withdrawn flag after event FeesAndRewardsWithdrawn

--- utils/hardcoded.ts
+++ utils/hardcoded.ts
@@ -2,7 +2,7 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { ProofOfHumanity as ProofOfHumanityContract } from "../generated/ProofOfHumanity/ProofOfHumanity";
 
 export const ProofOfHumanityAddress = Address.fromBytes(
-  Bytes.fromHexString("0x6cbEdC1920090EA4F28A38C1CD61c8D37b2cc323")
+  Bytes.fromHexString("0xa4AC94C4fa65Bb352eFa30e3408e64F72aC857bc")
 );
 
 export const ProofOfHumanity = ProofOfHumanityContract.bind(
