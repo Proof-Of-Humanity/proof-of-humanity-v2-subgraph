02b5660fccd45d22d6c134601aa10df6b791f42c
madhurMongia <madhurrajput1010@gmail.com>
2025-04-29 19:34:50 +0530

    feat: enhance CirclesAccount entity with crossChainRegistration and update event handling for member registration

--- mappings/circlesProxy.ts
+++ mappings/circlesProxy.ts
@@ -3,56 +3,47 @@ import {
   MemberRegistered,
   TrustRenewed,
 } from "../generated/ProofOfHumanityCirclesProxy/ProofOfHumanityCirclesProxy";
-import { CirclesAccount, Registration } from "../generated/schema";
+import { CirclesAccount, CrossChainRegistration, Registration } from "../generated/schema";
 
 export function handleMemberRegistered(event: MemberRegistered): void {
+
     log.info("handleMemberRegistered called: humanityID={} member={}", [
       event.params.humanityID.toHex(),
       event.params.member.toHex()
     ]);
-    const registration = Registration.load(event.params.humanityID);
-    if (!registration) {
-        log.error(
-          "Registration entity not found for humanityID={}",
-          [event.params.humanityID.toHex()]
-        );
-        return;
-    }
     let circlesAccount = new CirclesAccount(event.params.member);
-    circlesAccount.trustExpiryTime = registration.expirationTime;
-    circlesAccount.humanity = event.params.humanityID;
+    let registration = Registration.load(event.params.humanityID);
+
+    if (registration) {
+        circlesAccount.trustExpiryTime = registration.expirationTime;
+        circlesAccount.registration = registration.id;
+    } else {
+        let crossChainRegistration = CrossChainRegistration.load(event.params.humanityID);
+        if (crossChainRegistration) {
+            circlesAccount.trustExpiryTime = crossChainRegistration.expirationTime;
+            circlesAccount.crossChainRegistration = crossChainRegistration.id;
+        }
+    }
     circlesAccount.save();
-    log.info(
-      "CirclesAccount created id={} for humanityID={} expires={}",
-      [
-        event.params.member.toHex(),
-        event.params.humanityID.toHex(),
-        registration.expirationTime.toString()
-      ]
-    );
 }
 
 export function handleTrustRenewed(event: TrustRenewed): void {
-    log.info("handleTrustRenewed called: humanityID={} account={}", [
-      event.params.humanityID.toHex(),
-      event.params.account.toHex()
-    ]);
-    const registration = Registration.load(event.params.humanityID);
-    if (!registration) {
-        log.error(
-          "Registration entity not found for humanityID={}",
-          [event.params.humanityID.toHex()]
-        );
+    let circlesAccount = CirclesAccount.load(event.params.account);
+    if(!circlesAccount) {
+        log.error("CirclesAccount entity not found for account={}", [event.params.account.toHex()]);
         return;
     }
-    const circlesAccount = CirclesAccount.load(event.params.account);
-    if (!circlesAccount) {
-        log.error(
-          "CirclesAccount entity not found for account={}",
-          [event.params.account.toHex()]
-        );
-        return;
+    let registration = Registration.load(event.params.humanityID);
+
+    if (registration) {
+        circlesAccount.trustExpiryTime = registration.expirationTime;
+        circlesAccount.registration = registration.id;
+    } else {
+        let crossChainRegistration = CrossChainRegistration.load(event.params.humanityID);
+        if (crossChainRegistration) {
+            circlesAccount.trustExpiryTime = crossChainRegistration.expirationTime;
+            circlesAccount.crossChainRegistration = crossChainRegistration.id;
+        }
     }
-    circlesAccount.trustExpiryTime = registration.expirationTime;
     circlesAccount.save();
 }
