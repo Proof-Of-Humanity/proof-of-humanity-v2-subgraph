8fdaf42dfd1b8c4a8611759cda5a4c91b55bc936
madhurMongia <madhurrajput1010@gmail.com>
2025-08-09 00:42:35 +0530

    chore: regenerate subgraph.yaml and hardcoded.ts for chiado

--- utils/hardcoded.ts
+++ utils/hardcoded.ts
@@ -2,7 +2,7 @@ import { Address, Bytes } from "@graphprotocol/graph-ts";
 import { ProofOfHumanity as ProofOfHumanityContract } from "../generated/ProofOfHumanity/ProofOfHumanity";
 
 export const ProofOfHumanityAddress = Address.fromBytes(
-  Bytes.fromHexString("0xa4AC94C4fa65Bb352eFa30e3408e64F72aC857bc")
+  Bytes.fromHexString("0x3DED649Cc1E0a5D67614d6742C4919B10F0Aabe9")
 );
 
 export const ProofOfHumanity = ProofOfHumanityContract.bind(
